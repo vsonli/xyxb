327813ecfa6d9f68ec277ba52dc14332a0b461b6
Vson <1633979409@qq.com>
2019-06-06 12:21:56 +0800

    接口工程

--- common/apicom.py
+++ common/apicom.py
@@ -4,10 +4,11 @@
 # @Author :Vsonli
 import requests
 from common.myConf import conf
-
+import os
+path1 = os.path.abspath(os.path.dirname(os.getcwd()))+'\\'+'logs\\error.log'
 def get_token():
     '''获得督导的token'''
-    url = 'http://192.168.0.207:8081/xyxb/userCenter/login'
+    url = 'https://www.xuegean.com/xyxb/userCenter/login'
     head= {'content-type': 'application/x-www-form-urlencoded'}
     data = {
         'deviceCode': '481D3629d2d7978388365460dC5F91D2',
@@ -21,19 +22,26 @@ def get_token():
         sul = result.json()['data']['token']
     except:
         sul = result.text
+        try:
+            with open(path1, 'w') as f:
+                f.write('获取token接口发生了错误'+'            返回结果：'+sul)
+        except:
+            with open(path1, 'w') as f:
+                f.write(Exception)
+        print(Exception,'失败啦')
     return sul
 
 
 def get_student_token():
     '''获得学员的token'''
-    url = 'http://192.168.0.207:8081/xyxb/userCenter/login'
+    url = 'https://www.xuegean.com/xyxb/userCenter/login'
     head = {'content-type': 'application/x-www-form-urlencoded'}
     data = {
         'deviceCode': '481D3629d2d7978388365460dC5F91D2',
         'loginType': 1,
         'networkType': 0,
         'smsCode': '081588',
-        'userName': '8826'
+        'userName': '8830'
     }
     result = requests.post(url, headers=head, data=data, timeout=60)
     try:
@@ -51,35 +59,54 @@ def get_send_post(url,head,data,timeout):
     '''发送post请求'''
     try:
         result = requests.post(url, headers=head, data=data, timeout=timeout)
+        resu = result.text
+        item = url.split('/')[-1]
         response_time = result.elapsed.total_seconds()
     except:
+        try:
+            with open(path1, 'w') as f:
+                f.write(item + '接口发生了错误'+'            返回结果：'+resu)
+        except:
+            with open(path1, 'w') as f:
+                f.write(Exception)
         print(Exception,'失败啦')
     else:
-        item = url.split('/')[-1]
+        print(item+'请求参数：'+str(data))
         code = result.status_code
         if int(code) == 200 and result.json()['data'] != None:
-            resu = result.text
-            print(item+'接口通过','接口响应时间：'+str(response_time)+'秒',resu)
+            print(item+'接口访问通过','接口响应时间：'+str(response_time)+'秒')
+            print('返回结果为：' + resu)
+            print('                                ')
         else:
             resu = result.text
-            print(item+'接口失败','接口响应时间：'+str(response_time)+'秒',resu)
+            print(item+'接口访问失败','接口响应时间：'+str(response_time)+'秒')
+            print('返回结果为：'+resu)
+            print('                                ')
+
 
 def get_course(url,head,data,timeout):
     '''获得课程code'''
     try:
         result = requests.post(url, headers=head, data=data, timeout=timeout)
+        item = url.split('/')[-1]
+        resu = result.text
         code = result.status_code
         response_time = result.elapsed.total_seconds()
     except:
-        print(Exception)
-        print('失败啦')
+        try:
+            with open(path1, 'w') as f:
+                f.write(item + '接口发生了错误'+'            返回结果：'+resu)
+        except:
+            with open(path1, 'w') as f:
+                f.write(Exception)
+        print(Exception,'失败啦')
     else:
-        item = url.split('/')[-1]
         if int(code) == 200 and result.json()['data'] != None:
-            resu = result.text
-            print(item+'接口通过','接口响应时间：'+str(response_time)+'秒',resu)
+            print(item+'接口访问通过','接口响应时间：'+str(response_time)+'秒',resu)
+            print('                                ')
             courseCode = result.json()['data']['courseCode']
             return courseCode
         else:
             resu = result.text
-            print(item+'接口失败','接口响应时间：'+str(response_time)+'秒',resu)
+            print(item+'接口访问失败','接口响应时间：'+str(response_time)+'秒',resu)
+            print('                                ')
--- testcase/xyxbtest.py
+++ testcase/xyxbtest.py
@@ -9,62 +9,150 @@ teacher_tokens = get_token()
 teacher_heads = get_head(teacher_tokens)
 student_tokens = get_student_token()
 student_head = get_head(student_tokens)
+
 mydata={'groupCode':'196F0b9C1b282Da069DA0488b64834C0'}
 timeout=60
+def xyxb_live():
+    '''点击专栏'''
+    url = 'https://www.xuegean.com/xyxb/specialColumnCenter/getStudyFinish'
+    get_send_post(url,teacher_heads,mydata,timeout)
 
-'''点击专栏'''
-url = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/getStudyFinish'
-get_send_post(url,teacher_heads,mydata,timeout)
-
-url = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/groupDetail'
-get_send_post(url,teacher_heads,mydata,timeout)
-
-
-'''开启直播'''
-couse_data={'groupCode':'196F0b9C1b282Da069DA0488b64834C0','liveStatus':1}
-url = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/setLiveStatus'
-course = get_course(url,teacher_heads,couse_data,timeout)
-print('course值:'+str(course))
-
-url = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/groupDetail'
-get_send_post(url,teacher_heads,mydata,timeout)
-
-url = 'http://192.168.0.207:8081/xyxb/groupCenter/updateLastGroupSpeak'
-get_send_post(url,teacher_heads,mydata,timeout)
-
-couse_code = {'courseCode':course}
-print(couse_code)
-url = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/listGroupKey'
-get_send_post(url,teacher_heads,couse_code,timeout)
-time.sleep(3)
-url = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/listQuestion'
-get_send_post(url,teacher_heads,mydata,timeout)
+    url = 'https://www.xuegean.com/xyxb/specialColumnCenter/groupDetail'
+    get_send_post(url,teacher_heads,mydata,timeout)
 
 
-'''开启考勤'''
-url = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/startSupervisor'
-get_send_post(url,teacher_heads,mydata,timeout)
+    '''开启直播'''
+    couse_data={'groupCode':'196F0b9C1b282Da069DA0488b64834C0','liveStatus':1}
+    url = 'https://www.xuegean.com/xyxb/specialColumnCenter/setLiveStatus'
+    course = get_course(url,teacher_heads,couse_data,timeout)
+    print('course值:'+str(course))
 
-url = 'http://192.168.0.207:8081/xyxb/groupCenter/updateLastGroupSpeak'
-get_send_post(url,teacher_heads,mydata,timeout)
+    url = 'https://www.xuegean.com/xyxb/specialColumnCenter/groupDetail'
+    get_send_post(url,teacher_heads,mydata,timeout)
 
-url = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/userReckon'
-start_data={'duration':180,'groupCode':'196F0b9C1b282Da069DA0488b64834C0','courseCode':course}
-time.sleep(3)
-urls = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/listGroupKey'
-get_send_post(urls,teacher_heads,couse_code,timeout)
-time.sleep(3)
-'''结束直播'''
-urls = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/setLiveStatus'
-end_data={'groupCode':'196F0b9C1b282Da069DA0488b64834C0','liveStatus':0}
-get_send_post(urls,teacher_heads,end_data,timeout)
+    url = 'https://www.xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+    get_send_post(url,teacher_heads,mydata,timeout)
 
-urls = 'http://192.168.0.207:8081/xyxb/specialColumnCenter/userReckon'
-end_data={'groupCode':'196F0b9C1b282Da069DA0488b64834C0','duration':471,'courseCode':couse_data}
-get_send_post(urls,teacher_heads,end_data,timeout)
-time.sleep(3)
-urls = 'http://192.168.0.207:8081/xyxb/groupCenter/updateLastGroupSpeak'
-get_send_post(urls,teacher_heads,mydata,timeout)
+    couse_code = {'courseCode':course}
+    url = 'https://www.xuegean.com/xyxb/specialColumnCenter/listGroupKey'
+    get_send_post(url,teacher_heads,couse_code,timeout)
+    url = 'https://www.xuegean.com/xyxb/specialColumnCenter/listQuestion'
+    get_send_post(url,teacher_heads,mydata,timeout)
 
-urls = 'http://192.168.0.207:8081/xyxb/groupCenter/updateLastGroupSpeak'
-get_send_post(urls,teacher_heads,mydata,timeout)
+
+    '''开启考勤'''
+    url = 'https://www.xuegean.com/xyxb/specialColumnCenter/startSupervisor'
+    get_send_post(url,teacher_heads,mydata,timeout)
+
+    url = 'https://www.xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+    get_send_post(url,teacher_heads,mydata,timeout)
+
+    url = 'https://www.xuegean.com/xyxb/specialColumnCenter/userReckon'
+    start_data={'duration':180,'groupCode':'196F0b9C1b282Da069DA0488b64834C0','courseCode':course}
+    get_send_post(url, teacher_heads, start_data, timeout)
+
+    urls = 'https://www.xuegean.com/xyxb/specialColumnCenter/listGroupKey'
+    get_send_post(urls,teacher_heads,couse_code,timeout)
+
+
+
+    print('---------------------------以下为学员和督导之间的上下麦操作---------------------------------')
+    '''学员端操作'''
+    surl = 'https://www.xuegean.com/xyxb/specialColumnCenter/getStudyFinish'
+    get_send_post(surl,student_head,mydata,timeout)
+
+    surl = 'https://www.xuegean.com/xyxb/specialColumnCenter/groupDetail'
+    get_send_post(surl,student_head,mydata,timeout)
+
+    sdata={'useType':2,'groupCode':'196F0b9C1b282Da069DA0488b64834C0'}
+    surl = 'https://www.xuegean.com/xyxb/groupLiveTicket/isUseTicket'
+    get_send_post(surl,student_head,sdata,timeout)
+
+    surl = 'https://www.xuegean.com/xyxb/specialColumnCenter/listQuestion'
+    get_send_post(surl,student_head,mydata,timeout)
+
+    surl = 'https://www.xuegean.com/xyxb/specialColumnCenter/listQuestion'
+    get_send_post(surl,student_head,mydata,timeout)
+
+    surl = 'https://www.xuegean.com/xyxb/specialColumnCenter/listGroupKey'
+    get_send_post(surl,student_head,couse_code,timeout)
+
+    for i in range(6):
+        print('开始了核心小组的上下麦')
+        '''加入核心小组'''
+        groupKey = {'sort':i+1,'courseCode':course}
+        surl = 'https://www.xuegean.com/xyxb/specialColumnCenter/addGroupKey'
+        get_send_post(surl,student_head,groupKey,timeout)
+
+        surl = 'https://www.xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+        get_send_post(surl,student_head,mydata,timeout)
+
+
+        '''申请发言'''
+        editGroupKey = {'status':1,'courseCode':course}
+        surl = 'https://www.xuegean.com/xyxb/specialColumnCenter/editGroupKey'
+        get_send_post(surl,student_head,editGroupKey,timeout)
+
+        surl = 'https://www.xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+        get_send_post(surl,student_head,mydata,timeout)
+
+
+
+        '''督导操作开始
+            同意核心小组发言'''
+        editGroupKey = {'courseCode':course,'status':2,'userCode':'48D393a5aBc510B0a9601e3C1A0c9C9A'}
+        surl = 'https://www.xuegean.com/xyxb/specialColumnCenter/editGroupKey'
+        get_send_post(surl,teacher_heads,editGroupKey,timeout)
+
+        urls = 'https://www.xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+        get_send_post(urls,teacher_heads,mydata,timeout)
+        print('核心小组进行发言十秒')
+        time.sleep(10)
+
+        '''终止核心小组发言'''
+        editGroupKey = {'courseCode':course,'status':0,'userCode':'48D393a5aBc510B0a9601e3C1A0c9C9A'}
+        urls = 'https://www.xuegean.com/xyxb/specialColumnCenter/editGroupKey'
+        get_send_post(urls,teacher_heads,editGroupKey,timeout)
+
+        urls = 'https://www.xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+        get_send_post(urls,teacher_heads,mydata,timeout)
+
+        '''退出核心小组'''
+        quitGroupKey = {'courseCode':course,'sort':1}
+        urls = 'https://xuegean.com/xyxb/specialColumnCenter/quitGroupKey'
+        get_send_post(urls, student_head, quitGroupKey, timeout)
+
+        editGroupKey = {'courseCode': course, 'status': 0}
+        urls = 'https://xuegean.com/xyxb/specialColumnCenter/editGroupKey'
+        get_send_post(urls, student_head, editGroupKey, timeout)
+
+        urls = 'https://xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+        get_send_post(urls, student_head, mydata, timeout)
+
+        urls = 'https://xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+        get_send_post(urls, student_head, mydata, timeout)
+        print('结束了第'+str(i+1)+'次的核心小组的上下麦，准备开始下一轮')
+        time.sleep(5)
+    print('---------------------------学员和督导之间的上下麦操作结束---------------------------------')
+
+    print('正在持续开始直播，等待结束直播。。。。。。')
+    time.sleep(10)
+    '''结束直播'''
+    urls = 'https://www.xuegean.com/xyxb/specialColumnCenter/setLiveStatus'
+    end_data={'groupCode':'196F0b9C1b282Da069DA0488b64834C0','liveStatus':0}
+    get_send_post(urls,teacher_heads,end_data,timeout)
+
+    urls = 'https://www.xuegean.com/xyxb/specialColumnCenter/userReckon'
+    end_data={'groupCode':'196F0b9C1b282Da069DA0488b64834C0','duration':471,'courseCode':course}
+    get_send_post(urls,teacher_heads,end_data,timeout)
+    urls = 'https://www.xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+    get_send_post(urls,teacher_heads,mydata,timeout)
+
+    urls = 'https://www.xuegean.com/xyxb/groupCenter/updateLastGroupSpeak'
+    get_send_post(urls,teacher_heads,mydata,timeout)
+
+for i  in range(20):
+    print('这是第' + str(i + 1) + '次')
+    xyxb_live()
+    print('第' + str(i + 1) + '次执行结束，准备执行下一轮')
+    time.sleep(10)
