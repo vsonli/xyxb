9344375fc2e27ff3ff72e1c6acdc0470280803b5
Vson <1633979409@qq.com>
2019-07-12 16:05:01 +0800

    配置文件中新增方法，可写入内容到配置文件

--- common/myConf.py
+++ common/myConf.py
@@ -16,7 +16,25 @@ class RonfingObj(configparser.ConfigParser):
         r.read(os.path.join(CONF_DIR, 'env.conf'), encoding='utf8')
         switch = r.get('env','switch')
         if switch == '1':
+            self.file_path = os.path.join(CONF_DIR,'local.conf')
             self.read(os.path.join(CONF_DIR,'local.conf'), encoding='utf8')
         else:
+            self.file_path = os.path.join(CONF_DIR, 'official.conf')
             self.read(os.path.join(CONF_DIR, 'official.conf'), encoding='utf8')
+
+    def write_data(self,option,select,data):
+        '''
+        配置文件的写入
+        :param uption:
+        :param select:
+        :param data:    写入数据str类型
+        :return:
+        '''
+        self.set(option,select,data)
+        with open(self.file_path,'w') as f :
+            self.write(f)
 conf = RonfingObj()
+
+# a = 'x'
+# #在配置文件中，找到test_data这个类，找到user_num这个键，写入str(a)这个值
+# conf.write_data('test_data','user_num',str(a))
